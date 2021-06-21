ce824d3734bddef323acb15e5ec96a94fdc39800
FSantiago1987 <rawpower_br@hotmail.com>
2021-06-21 16:23:46 -0700

    Experience component

---
+++ src/components/Experience.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+function Experience() {
+    return (
+        <div className="experience">
+            <div className="d-flex justify-content-center my-5">
+                <h1>Experience</h1>
+            </div>
+            <div className="container experience-wrapper">
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2014-2015</h3>
+                        <p> Lore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsum
+                        Lore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsum
+                        Lore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsum</p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2016-2018</h3>
+                        <p> Lore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsum
+                        Lore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsum
+                        Lore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsum</p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2019-2020</h3>
+                        <p> Lore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsum
+                        Lore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsum
+                        Lore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsum</p>
+                    </div>
+                </div>
+                {/* - */}                    
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2020-2021</h3>
+                        <p> Lore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsum
+                        Lore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsum
+                        Lore apsumLore apsumLore apsumLore apsumLore apsumLore apsumLore apsum</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Experience;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,6 +22,9 @@ function Navbar() {
                 <a className="nav-link" href="about">About Me</a>
               </li>
               <li className="nav-item">
+                <a className="nav-link" href="projects">Experience</a>
+              </li>
+              <li className="nav-item">
                 <a className="nav-link" href="projects">Projects</a>
               </li>
               <li className="nav-item">
